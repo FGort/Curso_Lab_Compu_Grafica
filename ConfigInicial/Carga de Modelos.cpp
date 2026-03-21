c38837f7687ffd5c66750a70f0adddc16b920a5a
Fernanda Garcia Ortega <ferni2203@gmail.com>
2026-03-20 21:51:50 -0600

    Adjunto los archivos necesarios para hacer la practica 06

--- ConfigInicial/Carga de Modelos.cpp
+++ ConfigInicial/Carga de Modelos.cpp
@@ -1,5 +1,5 @@
-//Previo 06        García Ortega Fernanda 
-// Fecha de entrega:15/03/2026  320301159
+//Práctica 06        García Ortega Fernanda 
+// Fecha de entrega:20/03/2026  320301159
 // Std. Includes
 #include <string>
 
@@ -56,7 +56,7 @@ int main( )
     glfwWindowHint( GLFW_RESIZABLE, GL_FALSE );
     
     // Create a GLFWwindow object that we can use for GLFW's functions
-    GLFWwindow *window = glfwCreateWindow( WIDTH, HEIGHT, "Carga de modelos y camara sintetica - Previo 06 - Fernanda Garcia Ortega", nullptr, nullptr );
+    GLFWwindow *window = glfwCreateWindow( WIDTH, HEIGHT, "Carga de modelos y camara sintetica - Practica 06 - Fernanda Garcia Ortega", nullptr, nullptr );
     
     if ( nullptr == window )
     {
@@ -98,6 +98,18 @@ int main( )
     // Load models
     Model dog((char*)"Models/RedDog.obj");
     Model table((char*)"Models/coffee_table.obj");
+    Model cloud((char*)"Models/Low Poly Clouds.obj");
+    Model grass((char*)"Models/grass.obj");
+    Model arbour((char*)"Models/Arbour.obj");
+    Model tree((char*)"Models/Tree.obj");
+    Model hydrant((char*)"Models/hidrant_1.obj");
+    Model bench((char*)"Models/PARKTABLE.obj");
+    Model frisbee((char*)"Models/10505_Frisbee_v3_L3.obj");
+    Model corgi((char*)"Models/Corgi.obj");
+    Model play((char*)"Models/Playground_OBJ.obj");
+    Model pbench((char*)"Models/Classic_Garden_Bench.obj");
+    Model shiba((char*)"Models/Shiba.obj");
+    Model gtree((char*)"Models/GenTree_105_AE3D_03122023-F2.obj");
     glm::mat4 projection = glm::perspective( camera.GetZoom( ), ( float )SCREEN_WIDTH/( float )SCREEN_HEIGHT, 0.1f, 100.0f );
     
   
@@ -115,7 +127,7 @@ int main( )
         DoMovement();
 
         // Clear the colorbuffer
-        glClearColor(0.5f, 0.5f, 0.5f, 1.0f);
+        glClearColor(0.53f, 0.81f, 0.98f, 1.0f);
         glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
         shader.Use();
@@ -126,25 +138,181 @@ int main( )
 
         // Draw the loaded model
         glm::mat4 model(1);
-        glUniformMatrix4fv(glGetUniformLocation(shader.Program, "model"), 1, GL_FALSE, glm::value_ptr(model));
-        dog.Draw(shader);
-
-        model = glm::translate(model, glm::vec3(3.0f, 0.0f, 0.0f));
+        // Modelo de perro 
+        model = glm::translate(model, glm::vec3(-8.0f, 0.0f, 7.0f));
+        model = glm::rotate(model, glm::radians(25.0f), glm::vec3(0.0f, 1.0f, 0.0f));
         model = glm::scale(model, glm::vec3(2.0f, 2.0f, 2.0f));
         glUniformMatrix4fv(glGetUniformLocation(shader.Program, "model"), 1, GL_FALSE, glm::value_ptr(model));
         dog.Draw(shader);
 
-        //Modelo extra 
         model = glm::mat4(1);
-        model = glm::translate(model, glm::vec3(-2.0f, 0.0f, 0.0f));
-        model = glm::scale(model, glm::vec3(0.02f, 0.02f, 0.02f));
+        model = glm::translate(model, glm::vec3(-3.0f, -0.6f, 5.0f));
+        model = glm::scale(model, glm::vec3(0.009f, 0.009f, 0.009f));
+        glUniformMatrix4fv(glGetUniformLocation(shader.Program, "model"), 1, GL_FALSE, glm::value_ptr(model));
+        corgi.Draw(shader);
+
+        model = glm::mat4(1);
+        model = glm::translate(model, glm::vec3(2.7f, -0.5f, -1.0f));
+        model = glm::rotate(model, glm::radians(25.0f), glm::vec3(0.0f, -1.0f, 0.0f));
+        model = glm::scale(model, glm::vec3(2.5f, 2.5f, 2.5f));
+        glUniformMatrix4fv(glGetUniformLocation(shader.Program, "model"), 1, GL_FALSE, glm::value_ptr(model));
+        shiba.Draw(shader);
+
+        //Modelo de mesa 
+        model = glm::mat4(1);
+        model = glm::translate(model, glm::vec3(-5.0f, -0.4f, 6.0f));
+        model = glm::rotate(model, glm::radians(45.0f), glm::vec3(0.0f, 1.0f, 0.0f));
+        model = glm::scale(model, glm::vec3(0.04f, 0.04f, 0.04f));
         glUniformMatrix4fv(glGetUniformLocation(shader.Program, "model"), 1, GL_FALSE, glm::value_ptr(model));
         table.Draw(shader);
 
+        model = glm::mat4(1);
+        model = glm::translate(model, glm::vec3(7.0f, 0.7f, 6.0f));
+        model = glm::rotate(model, glm::radians(45.0f), glm::vec3(0.0f, 1.0f, 0.0f));
+        model = glm::scale(model, glm::vec3(0.9f, 0.9f, 0.9f));
+        glUniformMatrix4fv(glGetUniformLocation(shader.Program, "model"), 1, GL_FALSE, glm::value_ptr(model));
+        bench.Draw(shader);
 
+
+        //Modelo banca 
+        model = glm::mat4(1);
+        model = glm::translate(model, glm::vec3(8.0f, -0.1f, -5.0f));
+        model = glm::rotate(model, glm::radians(75.0f), glm::vec3(0.0f, -1.0f, 0.0f));
+        model = glm::scale(model, glm::vec3(1.5f, 1.5f, 1.5f));
+        glUniformMatrix4fv(glGetUniformLocation(shader.Program, "model"), 1, GL_FALSE, glm::value_ptr(model));
+        pbench.Draw(shader);
+
+        model = glm::mat4(1);
+        model = glm::translate(model, glm::vec3(-12.0f, -0.1f, -4.0f));
+        model = glm::rotate(model, glm::radians(75.0f), glm::vec3(0.0f, -1.0f, 0.0f));
+        model = glm::scale(model, glm::vec3(1.5f, 1.5f, 1.5f));
+        glUniformMatrix4fv(glGetUniformLocation(shader.Program, "model"), 1, GL_FALSE, glm::value_ptr(model));
+        pbench.Draw(shader);
+
+
+
+        //Modelo de nubes 
+        model = glm::mat4(1);
+        model = glm::translate(model, glm::vec3(0.0f, 6.0f, -30.0f));
+        model = glm::rotate(model, glm::radians(90.0f), glm::vec3(0.0f, 1.0f, 0.0f));
+        model = glm::scale(model, glm::vec3(0.6f, 0.6f, 0.6f));
+        glUniformMatrix4fv(glGetUniformLocation(shader.Program, "model"), 1, GL_FALSE, glm::value_ptr(model));
+        cloud.Draw(shader);
+
+        model = glm::mat4(1);
+        model = glm::translate(model, glm::vec3(-8.0f, 8.0f, -30.0f));
+        model = glm::rotate(model, glm::radians(90.0f), glm::vec3(0.0f, 1.0f, 0.0f));
+        model = glm::scale(model, glm::vec3(0.6f, 0.6f, 0.6f));
+        glUniformMatrix4fv(glGetUniformLocation(shader.Program, "model"), 1, GL_FALSE, glm::value_ptr(model));
+        cloud.Draw(shader);
+
+        model = glm::mat4(1);
+        model = glm::translate(model, glm::vec3(8.0f, 8.5f, -30.0f));
+        model = glm::rotate(model, glm::radians(90.0f), glm::vec3(0.0f, 1.0f, 0.0f));
+        model = glm::scale(model, glm::vec3(0.6f, 0.6f, 0.6f));
+        glUniformMatrix4fv(glGetUniformLocation(shader.Program, "model"), 1, GL_FALSE, glm::value_ptr(model));
+        cloud.Draw(shader);
+
+        model = glm::mat4(1);
+        model = glm::translate(model, glm::vec3(-16.0f, 6.0f, -30.0f));
+        model = glm::rotate(model, glm::radians(90.0f), glm::vec3(0.0f, 1.0f, 0.0f));
+        model = glm::scale(model, glm::vec3(0.6f, 0.6f, 0.6f));
+        glUniformMatrix4fv(glGetUniformLocation(shader.Program, "model"), 1, GL_FALSE, glm::value_ptr(model));
+        cloud.Draw(shader);
+
+        model = glm::mat4(1);
+        model = glm::translate(model, glm::vec3(16.0f, 7.0f, -30.0f));
+        model = glm::rotate(model, glm::radians(90.0f), glm::vec3(0.0f, 1.0f, 0.0f));
+        model = glm::scale(model, glm::vec3(0.6f, 0.6f, 0.6f));
+        glUniformMatrix4fv(glGetUniformLocation(shader.Program, "model"), 1, GL_FALSE, glm::value_ptr(model));
+        cloud.Draw(shader);
+
+        model = glm::mat4(1);
+        model = glm::translate(model, glm::vec3(-24.0f, 8.0f, -30.0f));
+        model = glm::rotate(model, glm::radians(90.0f), glm::vec3(0.0f, 1.0f, 0.0f));
+        model = glm::scale(model, glm::vec3(0.6f, 0.6f, 0.6f));
+        glUniformMatrix4fv(glGetUniformLocation(shader.Program, "model"), 1, GL_FALSE, glm::value_ptr(model));
+        cloud.Draw(shader);
+
+        //Modelo de pasto 
+        model = glm::mat4(1);
+        model = glm::translate(model, glm::vec3(-20.0f, -1.45f, -10.0f));
+        model = glm::scale(model, glm::vec3(10.0f, 10.0f, 10.0f));
+        glUniformMatrix4fv(glGetUniformLocation(shader.Program, "model"), 1, GL_FALSE, glm::value_ptr(model));
+        grass.Draw(shader);
+
+
+        //Modelo de techo 
+        model = glm::mat4(1);
+        model = glm::translate(model, glm::vec3(-3.0f, -0.3f, -5.0f));
+        model = glm::rotate(model, glm::radians(30.0f), glm::vec3(0.0f, -1.0f, 0.0f));
+        model = glm::scale(model, glm::vec3(2.5f, 2.5f, 2.5f));
+        glUniformMatrix4fv(glGetUniformLocation(shader.Program, "model"), 1, GL_FALSE, glm::value_ptr(model));
+        arbour.Draw(shader);
+
+        //Modelo de árboles 
+        model = glm::mat4(1);
+        model = glm::translate(model, glm::vec3(5.0f, -0.6f, -5.0f));
+        model = glm::rotate(model, glm::radians(45.0f), glm::vec3(0.0f, 1.0f, 0.0f));
+        model = glm::scale(model, glm::vec3(2.0f, 2.0f, 2.0f));
+        glUniformMatrix4fv(glGetUniformLocation(shader.Program, "model"), 1, GL_FALSE, glm::value_ptr(model));
+        tree.Draw(shader);
+
+        model = glm::mat4(1);
+        model = glm::translate(model, glm::vec3(-10.0f, -0.7f, -5.0f));
+        model = glm::rotate(model, glm::radians(45.0f), glm::vec3(0.0f, 1.0f, 0.0f));
+        model = glm::scale(model, glm::vec3(2.0f, 2.0f, 2.0f));
+        glUniformMatrix4fv(glGetUniformLocation(shader.Program, "model"), 1, GL_FALSE, glm::value_ptr(model));
+        tree.Draw(shader);
+
+        model = glm::mat4(1);
+        model = glm::translate(model, glm::vec3(10.0f, -0.7f, -5.0f));
+        model = glm::rotate(model, glm::radians(45.0f), glm::vec3(0.0f, 1.0f, 0.0f));
+        model = glm::scale(model, glm::vec3(2.0f, 2.0f, 2.0f));
+        glUniformMatrix4fv(glGetUniformLocation(shader.Program, "model"), 1, GL_FALSE, glm::value_ptr(model));
+        tree.Draw(shader);
+
+        model = glm::mat4(1);
+        model = glm::translate(model, glm::vec3(-10.0f, -0.6f, 3.0f));
+        model = glm::rotate(model, glm::radians(45.0f), glm::vec3(0.0f, 1.0f, 0.0f));
+        model = glm::scale(model, glm::vec3(0.8f, 0.8f, 0.8f));
+        glUniformMatrix4fv(glGetUniformLocation(shader.Program, "model"), 1, GL_FALSE, glm::value_ptr(model));
+        gtree.Draw(shader);
+
+        model = glm::mat4(1);
+        model = glm::translate(model, glm::vec3(12.0f, -0.6f, 3.0f));
+        model = glm::rotate(model, glm::radians(45.0f), glm::vec3(0.0f, 1.0f, 0.0f));
+        model = glm::scale(model, glm::vec3(0.8f, 0.8f, 0.8f));
+        glUniformMatrix4fv(glGetUniformLocation(shader.Program, "model"), 1, GL_FALSE, glm::value_ptr(model));
+        gtree.Draw(shader);
+
+        //Modelo de hidrante 
+        model = glm::mat4(1);
+        model = glm::translate(model, glm::vec3(-2.0f, 0.2f, 4.0f));
+        model = glm::scale(model, glm::vec3(0.4f, 0.4f, 0.4f));
+        glUniformMatrix4fv(glGetUniformLocation(shader.Program, "model"), 1, GL_FALSE, glm::value_ptr(model));
+        hydrant.Draw(shader);
+
+        //Modelo de frisbee
+        model = glm::mat4(1);
+        model = glm::translate(model, glm::vec3(0.8f, 1.7f, 1.0f));
+        model = glm::rotate(model, glm::radians(90.0f), glm::vec3(-1.0f, 0.0f, 0.0f));
+        model = glm::scale(model, glm::vec3(0.05f, 0.05f, 0.05f));
+        glUniformMatrix4fv(glGetUniformLocation(shader.Program, "model"), 1, GL_FALSE, glm::value_ptr(model));
+        frisbee.Draw(shader);
+
+        //Modelo de juegos de parque 
+        model = glm::mat4(1);
+        model = glm::translate(model, glm::vec3(16.0f, -1.0f, -4.0f));
+        model = glm::rotate(model, glm::radians(45.0f), glm::vec3(0.0f, -1.0f, 0.0f));
+        model = glm::scale(model, glm::vec3(1.8f, 1.8f, 1.8f));
+        glUniformMatrix4fv(glGetUniformLocation(shader.Program, "model"), 1, GL_FALSE, glm::value_ptr(model));
+        play.Draw(shader);
         // Swap the buffers
         glfwSwapBuffers( window );
     }
+
+    
     
     glfwTerminate( );
     return 0;
